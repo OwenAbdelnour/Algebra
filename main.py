7445278a12a041fb5a70fe9c1cf2168878c70d50
OwenAbdelnour <owenabdelnour@gmail.com>
2022-01-15 17:51:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 equation = input("Equation")
-divide = 0
+equal = 0
 
 terms = [[[]]]
 for x in range(2):
-  for a in range(divide, len(equation)):
+  for a in range(equal, len(equation)):
     if x==1:
-      if divide==0:
+      if equal==0:
         break
       elif len(terms)==1:
         terms.append([[]])
     elif equation[a] == "=":
-      divide = a
+      equal = a
       break
     if equation[a] in "+*/":
       terms[x].append([])
     elif equation[a]!="=":
       terms[x][len(terms[x])-1].append(equation[a])
 
-for x in range(2-0**divide):
+for x in range(len(terms)):
   for a in range(len(terms[x])):
     co, num_spot, var = 0, 1, ""
     for b in range(len(terms[x][a])-1,-1,-1):
@@ -43,31 +43,41 @@ for x in range(2-0**divide):
       terms[x][a].insert(1, var)
 
 def combine():
-  for x in range(2-0**divide):
+  for x in range(len(terms)):
     a = 0
     while a<len(terms[x]):
       b = 0
       while b<len(terms[x]):
         if a != b:
           if len(terms[x][a])==1 and len(terms[x][b])==1:
-            terms[x].append([terms[x][a][0]+terms[x][b][0]])
+            if terms[x][a][0]+terms[x][b][0] != 0:
+              terms[x].append([terms[x][a][0]+terms[x][b][0]])
             terms[x].pop(a)
             b -= 1
             terms[x].pop(b)
           elif len(terms[x][a])>1 and len(terms[x][b])>1 and terms[x][a][1]==terms[x][b][1]:
-            terms[x].append([terms[x][a][0]+terms[x][b][0], terms[x][a][1]])
+            if terms[x][a][0]+terms[x][b][0] != 0:
+              terms[x].append([terms[x][a][0]+terms[x][b][0], terms[x][a][1]])
             terms[x].pop(a)
             b -= 1
             terms[x].pop(b)
         b += 1
       a += 1
+
+def add_sub(term):
+  for x in range(len(terms)):
+    terms[x].append([term])
+
+def multiply(term):
+  pass
+
+def divide(term):
+  pass
+
 combine()
-print(terms)
-def add_sub(term1, term2):
-  pass
-
-def multiply(term1, term2):
-  pass
-
-def divide(term1, term2):
-  pass
+while True:
+  task = input("Run: ")
+  term = int(input("Term: "))
+  if task == "add_sub":
+    add_sub(term)
+  print(terms)
