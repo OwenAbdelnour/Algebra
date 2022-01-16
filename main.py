09881360ad13642ec5bf19fb32b54c24147b3340
OwenAbdelnour <owenabdelnour@gmail.com>
2022-01-15 23:16:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,63 @@
 equation = input("Equation")
 equal = 0
 
-terms = [[[]]]
+terms = [[[[],"","+"]], [[[],"","+"]]]
 for x in range(2):
-  for a in range(equal, len(equation)):
-    if x==1:
-      if equal==0:
+  a = equal+x
+  while a < len(equation):
+    if len(terms)==2:
+      y = x
+    else:
+      y = -1
+    if x==1 and equal == 0:
         break
-      elif len(terms)==1:
-        terms.append([[]])
     elif equation[a] == "=":
       equal = a
       break
-    if equation[a] in "+*/":
-      terms[x].append([])
-    elif equation[a]!="=":
-      terms[x][len(terms[x])-1].append(equation[a])
-
-for x in range(len(terms)):
-  for a in range(len(terms[x])):
-    co, num_spot, var = 0, 1, ""
-    for b in range(len(terms[x][a])-1,-1,-1):
-      if terms[x][a][b] in "123456789":
-        if num_spot==1:
-          co += int(terms[x][a][b])
-        else:
-          co += int(terms[x][a][b])*num_spot
-        terms[x][a].remove(terms[x][a][b])
-        num_spot *= 10
-      elif terms[x][a][b] == "0":
-        terms[x][a].remove(terms[x][a][b])
-        num_spot *= 10
-      elif terms[x][a][b] == "-":
-        terms[x][a].remove(terms[x][a][b])
-        co *= -1
+    if equation[a] in "+-":
+      if (len(terms[y][-1][0]) == 0 or terms[y][-1][-1][1] != "+") and equation[a+1] != "(":
+        if terms[y][-1][2] == "+":
+          if equation[a] == "-":
+            terms[y][-1][2] = "-"
+        elif equation[a-1] not in "+-":
+          terms[y].append([[],"",equation[a]])
+        elif equation[a-1]+equation[a] == "+-":
+          terms[y][-1] = [],"","-"
+        elif equation[a-1]+equation[a] == "--":
+          terms[y][-1] = [],"","+"
       else:
-        var += terms[x][a][b]
-        terms[x][a].remove(terms[x][a][b])
-    if co!=0:
-      terms[x][a].insert(0, co)
-    if var != "":
-      terms[x][a].insert(1, var)
-
-def combine():
-  for x in range(len(terms)):
-    a = 0
-    while a<len(terms[x]):
-      b = 0
-      while b<len(terms[x]):
-        if a != b:
-          if len(terms[x][a])==1 and len(terms[x][b])==1:
-            if terms[x][a][0]+terms[x][b][0] != 0:
-              terms[x].append([terms[x][a][0]+terms[x][b][0]])
-            terms[x].pop(a)
-            b -= 1
-            terms[x].pop(b)
-          elif len(terms[x][a])>1 and len(terms[x][b])>1 and terms[x][a][1]==terms[x][b][1]:
-            if terms[x][a][0]+terms[x][b][0] != 0:
-              terms[x].append([terms[x][a][0]+terms[x][b][0], terms[x][a][1]])
-            terms[x].pop(a)
-            b -= 1
-            terms[x].pop(b)
-        b += 1
-      a += 1
-
-def add_sub(term):
-  for x in range(len(terms)):
-    terms[x].append([term])
-
-def multiply(term):
-  pass
-
-def divide(term):
-  pass
-
-combine()
-while True:
-  task = input("Run: ")
-  term = int(input("Term: "))
-  if task == "add_sub":
-    add_sub(term)
-  print(terms)
+        if terms[y][-1][-1][1]+equation[a] == "+-":
+          terms[y][-1][-1][1] = "-"
+        elif terms[y][-1][-1][1]+equation[a] == "--":
+          terms[y][-1][-1][1] = "+"
+    elif equation[a] in "*/":
+      terms[y][-1][0].append(equation[a])
+      terms[y][-1].append(["","+"])
+    elif equation[a] == "(":
+      par_enter = equation[a-1]
+      terms.append([[[],"","+"]])
+    elif equation[a] == ")":
+      if par_enter == "+":
+        terms[x].append(terms[y][0])
+      elif par_enter == "-":
+        terms[x].append([["*"], "", "-1", terms[y][0]])
+      elif par_enter in "*/":
+        del terms[x][-1][-1]
+        terms[x][-1].append(terms[y][0])
+      else:
+        terms[x][-1][0].append("*")
+        terms[x][-1].append(terms[y][0])
+      del terms[y]
+    elif equation[a] != "=":
+      if len(terms[y][-1][0])==0:
+        if equation[a] in "0123456789":
+          terms[y][-1][2] = terms[y][-1][2]+equation[a]
+        else:
+          terms[y][-1][1] = terms[y][-1][1]+equation[a]
+      else:
+        if equation[a] in "0123456789":
+          terms[y][-1][-1][1] = terms[y][-1][-1][1]+equation[a]
+        else:
+          terms[y][-1][-1][0] = terms[y][-1][-1][0]+equation[a]
+    a += 1
+print(terms)
